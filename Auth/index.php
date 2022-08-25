06f9c2e69a2af17b03fcefe68fca4f926cef58bc
Carliinhozz <103829442+Carliinhozz@users.noreply.github.com>
2022-08-25 11:27:45 -0300

    Update index.php

--- Auth/index.php
+++ Auth/index.php
@@ -4,10 +4,10 @@
   $router=include_once __DIR__.'/web.php';
   use Carlos\Auth\App\Application;
   use Carlos\Auth\Models\Model;
-  Model::blindConnection(new SQLite3(__DIR__.'banco.db'));
-  Model::create_table("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)");
-  Model::create_table("CREATE TABLE IF NOT EXISTS books(title TEXT)");
-  Model::create_table("CREATE TABLE IF NOT EXISTS loans(user TEXT, book TEXT)");
+  //Model::blindConnection(new SQLite3(__DIR__.'banco.db'));
+  //Model::create_table("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)");
+  //Model::create_table("CREATE TABLE IF NOT EXISTS books(title TEXT)");
+  //Model::create_table("CREATE TABLE IF NOT EXISTS loans(user TEXT, book TEXT)");
 
 
 
